7c6839124abf160a90533671ba14d4206c720663
Wenlin Fang <42773608+wenlinf@users.noreply.github.com>
2023-09-12 12:34:58 -0400

    Update confirmation_rate.sql

--- confirmation_rate.sql
+++ confirmation_rate.sql
@@ -49,3 +49,7 @@ right join (
   group by s.user_id
 ) as total_group 
 on confirmed_group.user_id = total_group.user_id;
+
+# Found a much more elegant solution on Leetcode:
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
